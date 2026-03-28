9b40f89db1bf18cf97a14c08f44c0bbb9e7da8e9
Josua Müller <hypnotox@pm.me>
2026-03-28 13:48:47 +0100

    chore: run toml encode and decode tests together

--- tests/run.sh
+++ tests/run.sh
@@ -10,9 +10,5 @@ fi
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo "Running decoder tests..."
-toml-test test -decoder "php ${SCRIPT_DIR}/toml-decoder.php"
-
-echo ""
-echo "Running encoder tests..."
+echo "Running conformance tests..."
 toml-test test -decoder "php ${SCRIPT_DIR}/toml-decoder.php" -encoder "php ${SCRIPT_DIR}/toml-encoder.php"
